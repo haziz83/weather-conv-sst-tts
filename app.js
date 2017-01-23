4bd761080bc5872109f074b99ee33ba08e056306
Ahmed Azraq <aazraq@eg.ibm.com>
2017-01-23 00:20:46 -0600

    Integration with weather

--- app.js
+++ app.js
@@ -33,38 +33,42 @@ var weatherCompanyEndpoint = vcap.weatherinsights[0].credentials.url;
 
 // Create the service wrapper
 var conversation = new Conversation({
-  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  // username: '<username>',
-  // password: '<password>',
-  url: 'https://gateway.watsonplatform.net/conversation/api',
-  version_date: '2016-10-21',
-  version: 'v1'
+	// If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+	// After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+	// username: '<username>',
+	// password: '<password>',
+	url: 'https://gateway.watsonplatform.net/conversation/api',
+	version_date: '2016-10-21',
+	version: 'v1'
 });
 
 // Endpoint to be call from the client side
 app.post('/api/message', function(req, res) {
-  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-  if (!workspace || workspace === '<workspace-id>') {
-    return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-      }
-    });
-  }
-  var payload = {
-    workspace_id: workspace,
-    context: req.body.context || {},
-    input: req.body.input || {}
-  };
+	var workspace = process.env.WORKSPACE_ID || '5cea3898-6427-426f-bcb1-3ae3655e5a3f';
+	// var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+	if (!workspace || workspace === '<workspace-id>') {
+		return res.json({
+			'output': {
+				'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+			}
+		});
+	}
+	var payload = {
+		workspace_id: workspace,
+		context: req.body.context || {},
+		input: req.body.input || {}
+	};
 
-  // Send the input to the conversation service
-  conversation.message(payload, function(err, data) {
-    if (err) {
-      return res.status(err.code || 500).json(err);
-    }
-    return res.json(updateMessage(payload, data));
-  });
+	// Send the input to the conversation service
+	conversation.message(payload, function(err, data) {
+		if (err) {
+			return res.status(err.code || 500).json(err);
+		}
+		updateMessage(payload, data, function(response){
+			return res.json(response);
+		});
+		
+	});
 });
 
 /**
@@ -72,12 +76,12 @@ app.post('/api/message', function(req, res) {
  * @param  {Object} city The target city
  * @return {Object} location The latitude and longitude of the city
  */
-function getLocationCoordinatesForCity(city){
+function getLocationCoordinatesForCity(city) {
 	var location = {};
-	if(city==='Cairo'){
+	if (city === 'Cairo') {
 		location.latitude = '30.0444';
 		location.longitude = '31.2357';
-	} else if(city==='NYC'){
+	} else if (city === 'NYC') {
 		location.latitude = '40.7128';
 		location.longitude = '74.0059';
 	}
@@ -89,9 +93,9 @@ function getLocationCoordinatesForCity(city){
  * @param  {Object} city The target city
  * @return {Object}      Weather Forecast for the specified city.
  */
-function getWeatherForecastForCity(location,  callback) {
+function getWeatherForecastForCity(location, callback) {
 	var options = {
-		url: weatherCompanyEndpoint + '/api/weather/v1/geocode/' + location.latidude + '/' + location.longitude + '/forecast/daily/3day.json'
+		url: weatherCompanyEndpoint + '/api/weather/v1/geocode/' + location.latitude + '/' + location.longitude + '/forecast/daily/3day.json'
 	};
 	request(
 		options,
@@ -107,7 +111,7 @@ function getWeatherForecastForCity(location,  callback) {
 	);
 };
 
- 
+
 
 /**
  * Updates the response text using the intent confidence
@@ -115,30 +119,19 @@ function getWeatherForecastForCity(location,  callback) {
  * @param  {Object} response The response from the Conversation service
  * @return {Object}          The response with the updated message
  */
-function updateMessage(input, response) {
-  var responseText = null;
-  if (!response.output) {
-    response.output = {};
-  } else {
-    return response;
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
-    }
-  }
-  response.output.text = responseText;
-  return response;
+function updateMessage(input, response, callback) {
+	var responseText = null;
+	if (!response.output) {
+		response.output = {};
+	} else if (response.entities.length > 0 && response.entities[0].entity === 'city') {
+		var location = getLocationCoordinatesForCity(response.entities[0].value);		
+		getWeatherForecastForCity(location, function(e, weatherOutput) {
+			response.output.text[0] = weatherOutput;
+			callback(response);
+
+		});
+	} 
+	callback(response);
 }
 
 module.exports = app;
